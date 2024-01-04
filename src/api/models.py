333b5279c411a208886d0335731e59adaadef903
Christa Kelly <christadkelly@gmail.com>
2024-01-04 21:08:29 +0000

    started on models

--- src/api/models.py
+++ src/api/models.py
@@ -1,12 +1,16 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import func
 
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    username = db.Column(db.String(120), unique=True, nullable=False)
+
+    favorites = db.relationship("Favorites", backref="user_favorites")
 
     def __repr__(self):
         return f'<User {self.email}>'
@@ -15,5 +19,64 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            "username": self.username,
+            "favorites": list(map(lambda x: x.serialize(), self.favorites))
+        }
+    
+class Favorites(db.Model):
+    __tablename__ = 'favorites'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    category = db.Column(db.String(120), nullable=False)
+    category_id = db.Column(db.Integer)
+
+    def __repr__(self):
+        return f'<Favorites {self.id}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "category": self.category,
+            "category.id": self.category_id
+        }
+    
+class Post(db.Model):
+    __tablename__ = 'post'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    title = db.Column(db.String(120), nullable=False)
+    body = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
+    modified_at = db.Column(db.DateTime(timezone=True), onupdate=func.now())
+
+    def __repr__(self):
+        return f'<Post {self.title}>'
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "body": self.body,
+            "created": self.created_at,
+            "modified": self.modified_at
+        }
+    
+class Comment(db.Model):
+    _tablename__ = 'comment'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id'))
+    comment = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
+    modified_at = db.Column(db.DateTime(timezone=True), onupdate=func.now())
+
+    def __repr__(self):
+        return f'<Comment {self.body}>'
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "comment": self.comment,
+            "created": self.created_at,
+            "modified": self.modified_at
         }
